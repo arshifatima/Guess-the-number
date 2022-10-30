8c794768a2e087075dc6a17a2c1ff1f07a3e9539
arshifatima <arshifatima8002@gmail.com>
2022-10-31 01:25:30 +0530

    Java Annotations

--- core/src/main/java/tuta/nota/Main.java
+++ core/src/main/java/tuta/nota/Main.java
@@ -3,17 +3,18 @@ package tuta.nota;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    private static final String CONFIG_LOCATION = "beans.xml";
+
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         log.info("Guess the Number");
         //create context container
         ConfigurableApplicationContext context =
-                new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
         //get number bean generator from context(container)
         NumberGenerator numberGenerator =
